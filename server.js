e7103a1cdd10ca59980019a339edfb82af0acd9d
robyntaylor94 <robynrylana.tt@gmail.com>
2026-01-09 18:53:22 -0500

    moved index.html to root for github pages

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 // Fallback route (optional but nice)
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "/", "index.html"));
 });
 
 app.listen(PORT, () => {
